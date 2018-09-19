8a0cdbdf65d5fa6125259508e544173b9501fe44
Natan <nferreira@primeiramao.com.br>
2018-09-19 12:43:29 -0300

    criando a lista de games

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # top10
 Teste Rede Bandeirantes
+
+##Iniciando
+#unix
+Para iniciar rodar a aplicação entre na pasta (cd top10 ) e execute o seguinte comando:
+ -  ng serve
+
+Podera ver a aplicação rodando no link abaixo
+ http://localhost:4200
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GamesComponent } from './games/games.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GamesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
